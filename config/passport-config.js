f9478730ef56367a35d7efc7fb9a5075afb6c7d0
Njuguna85 <dennix.njuguna85@gmail.com>
2020-10-16 03:55:52 +0300

    passport config

--- config/passport-config.js
+++ config/passport-config.js
@@ -9,7 +9,7 @@ function initialize(passport) {
     passport.use(new GoogleStatergy({
                 clientID: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: '/auth/google/callback'
+                callbackURL: 'https://gis.predictiveanalytics.co.ke/auth/google/callback'
             },
             // access the user details from the profile
             async(accessToken, refreshToken, profile, done) => {
