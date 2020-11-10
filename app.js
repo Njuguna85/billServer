86f0867d3cabdb6bc24270e4893ab67a38e00c4b
Njuguna85 <dennix.njuguna85@gmail.com>
2020-11-10 20:42:51 +0300

    testing git

--- app.js
+++ app.js
@@ -1,64 +1,65 @@
-const path = require('path')
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
-const passport = require('passport')
-const session = require('express-session');
-const dotenv = require('dotenv')
-    // set the path of config file
-dotenv.config({ path: './config/config.env' })
-const billboards = require('./routes/billboards')
-const exhbs = require('express-handlebars');
-const morgan = require('morgan');
-const index = require('./routes/index');
-require('./models/models');
-const logger = require('./controllers/logger');
+const passport = require("passport");
+const session = require("express-session");
+const dotenv = require("dotenv");
+// set the path of config file
+dotenv.config({ path: "./config/config.env" });
+const billboards = require("./routes/billboards");
+const exhbs = require("express-handlebars");
+const morgan = require("morgan");
+const index = require("./routes/index");
+require("./models/models");
+const logger = require("./controllers/logger");
 
 // use the json payload for body requests
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 // use morgan to log request
-app.use(morgan('combined', { stream: logger.stream }));
+app.use(morgan("combined", { stream: logger.stream }));
 
-
-// passport configuration 
+// passport configuration
 // pass the passport var by reference
-require('./config/passport-config')(passport);
+require("./config/passport-config")(passport);
 
 // use express js sessinons
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     // dont save a session if nothing is modified
     resave: false,
     // dont create a session until something is stored
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 
 // add passport middleware
-// initialize and passports sessions 
+// initialize and passports sessions
 app.use(passport.initialize());
 app.use(passport.session());
 
 // handlebars set it to use .hbs extension
 // set the default layout to main
-app.engine('.hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }));
-app.set('view engine', '.hbs');
+app.engine(".hbs", exhbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
-// set the static folder 
-app.use(express.static(path.join(__dirname, 'public')))
+// set the static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 // routes
-app.use('/auth', require('./routes/auth'))
-app.use('/', index)
-app.use('/roles', require('./routes/roles'))
+app.use("/auth", require("./routes/auth"));
+app.use("/", index);
+app.use("/roles", require("./routes/roles"));
 
-// set the access the route of billboards 
-app.use('/api/billboards', billboards)
+// set the access the route of billboards
+app.use("/api/billboards", billboards);
 
-app.use('/api/businesses', require('./routes/business'));
+app.use("/api/businesses", require("./routes/business"));
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Listening to port ${port}`);
-})
+  console.log(`Listening to port ${port}`);
+});
